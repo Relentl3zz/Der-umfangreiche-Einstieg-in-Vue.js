1dd3a6d22c9f7d16bfd677e7477949874ca8a7ca
b.derksen@mail.de <b.derksen@mail.de>
2020-05-17 16:22:46 +0200

    Lektion 11: Mehrere ToDos mit v-for rendern

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <h1>
-    Hello World
-    <span v-bind:style="{ color: favoriteColor }"> {{ message }} </span>
-    <p v-bind:style="{ color: errorColor }" v-if="errorColor === 'red'">
-      FEHLER
-    </p>
-    <p v-else v-bind:style="{ color: errorColor }">ERFOLG</p>
-  </h1>
-</template>
+  <div>
+    <h1>
+      Hello World
+      <span v-bind:style="{ color: favoriteColor }"> {{ message }} </span>
+      <p v-bind:style="{ color: errorColor }" v-if="errorColor === 'red'">
+        FEHLER
+      </p>
+      <p v-else v-bind:style="{ color: errorColor }">ERFOLG</p>
+    </h1>
 
+    <div v-for="(todo, index) in todos" :key="index">{{ todo.title }}</div>
+  </div>
+</template>
 <script>
 export default {
   data() {
@@ -16,6 +19,11 @@ export default {
       message: "Benny",
       favoriteColor: "green",
       errorColor: "green",
+      todos: [
+        { title: "HTML, CSS & JS lernen" },
+        { title: "Vue lernen" },
+        { title: "Next facebook bauen" },
+      ],
     };
   },
 };
