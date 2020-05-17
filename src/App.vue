f8cd9d47110a8c31b206ddefbff9e14bd8ec9a11
b.derksen@mail.de <b.derksen@mail.de>
2020-05-17 16:11:40 +0200

    Lektion 10: Error Messages mit v-if

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <h1>
     Hello World
     <span v-bind:style="{ color: favoriteColor }"> {{ message }} </span>
+    <p v-bind:style="{ color: errorColor }" v-if="errorColor === 'red'">
+      FEHLER
+    </p>
+    <p v-else v-bind:style="{ color: errorColor }">ERFOLG</p>
   </h1>
 </template>
 
@@ -11,6 +15,7 @@ export default {
     return {
       message: "Benny",
       favoriteColor: "green",
+      errorColor: "green",
     };
   },
 };
