628438b7c9359dacb21eb004ef194c157aa55d09
b.derksen@mail.de <b.derksen@mail.de>
2020-05-21 13:03:34 +0200

    Lektion 20: ToDos anhand der ID löschen

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="(todo, index) in todos" :key="index">{{ todo.title }}</div>
+    <input v-on:keyup.enter="addToDo" />
+    <div v-for="(todo, index) in todos" :key="index">
+      {{ todo.title }} <button v-on:click="deleteToDo(todo.id)">X</button>
+    </div>
   </div>
 </template>
 
@@ -9,12 +12,24 @@ export default {
   data() {
     return {
       todos: [
-        { title: "HTML, CSS & JS lernen" },
-        { title: "Vue lernen" },
-        { title: "Next facebook bauen" },
+        { id: 1, title: "HTML, CSS & JS lernen" },
+        { id: 2, title: "Vue lernen" },
+        { id: 3, title: "Next facebook bauen" },
       ],
-      counter: 0,
+      counter: 4,
     };
+  },
+  methods: {
+    addToDo(e) {
+      this.todos.push({ id: this.counter++, title: e.target.value });
+      e.target.value = "";
+    },
+    deleteToDo(id) {
+      this.todos.splice(
+        this.todos.findIndex((item) => item.id === id),
+        1
+      );
+    },
   },
 };
 </script>
