05de7a636aebc0de0b52743b697e55922b96ce4f
b.derksen@mail.de <b.derksen@mail.de>
2020-05-21 14:01:19 +0200

    Lektion 21: doneToDos in eigener List mit computed

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <input v-on:keyup.enter="addToDo" />
-    <div v-for="(todo, index) in todos" :key="index">
-      <button v-on:click="deleteToDo(todo.id)">X</button>
+
+    <h2>Active ToDos</h2>
+
+    <div v-for="todo in activeToDos" :key="todo.id">
       <input type="checkbox" v-model="todo.done" /> {{ todo.title }}
+      <button v-on:click="deleteToDo(todo.id)">X</button>
+    </div>
+
+    <br />
+
+    <h2>Done ToDos</h2>
+
+    <div v-for="todo in doneToDos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" /> {{ todo.title }}
+      <button v-on:click="deleteToDo(todo.id)">X</button>
     </div>
   </div>
 </template>
@@ -16,6 +28,7 @@ export default {
         { id: 1, title: "HTML, CSS & JS lernen", done: false },
         { id: 2, title: "Vue lernen", done: false },
         { id: 3, title: "Next facebook bauen", done: true },
+        { id: 4, title: "Vue aufgabe erledigt", done: true },
       ],
       counter: 4,
     };
@@ -30,6 +43,10 @@ export default {
         this.todos.findIndex((item) => item.id === id),
         1
       );
+    },
+    computed: {},
+    doneToDos() {
+      return this.todos.filter((item) => item.done);
     },
   },
 };
