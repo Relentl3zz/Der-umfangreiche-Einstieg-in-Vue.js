ef2aea3785c336d4d97a2164f480b7c93486b43d
b.derksen@mail.de <b.derksen@mail.de>
2020-05-21 12:09:32 +0200

    Lektion 17: Die erste Komponente ToDoList

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,13 @@
 <template>
   <div>
-    <h1>
-      Hello World
-      <span :style="{ color: favoriteColor }"> {{ message }} </span>
-      <p :style="{ color: errorColor }" v-if="errorColor === 'red'">
-        FEHLER
-      </p>
-      <p v-else :style="{ color: errorColor }" v-on:click="swapColor">
-        ERFOLG
-      </p>
-    </h1>
-
-    <div v-for="(todo, index) in todos" :key="index">{{ todo.title }}</div>
-    <button @click="resetToDos">Reset</button>
-    <br />
-    <br />
-    <input :value="message" v-on:input="setMessage" />
+    <ToDoList />
   </div>
 </template>
 <script>
+import ToDoList from "@/views/ToDoList";
 export default {
-  data() {
-    return {
-      message: "Hey, wie gehts dir",
-      favoriteColor: "green",
-      errorColor: "green",
-      todos: [
-        { title: "HTML, CSS & JS lernen" },
-        { title: "Vue lernen" },
-        { title: "Next facebook bauen" },
-      ],
-      counter: 0,
-    };
-  },
-  methods: {
-    swapColor() {
-      let colorArray = ["blue", "green", "orange", "magenta", "cyan"];
-      this.counter++;
-      if (colorArray.length === this.counter) this.counter = 0;
-      this.errorColor = colorArray[this.counter];
-    },
-    setMessage(e) {
-      console.log(e);
-      this.message = e.target.value;
-    },
-    resetToDos() {
-      this.todos = [];
-    },
+  components: {
+    ToDoList,
   },
 };
 </script>
---
+++ src/views/ToDoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(todo, index) in todos" :key="index">{{ todo.title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        { title: "HTML, CSS & JS lernen" },
+        { title: "Vue lernen" },
+        { title: "Next facebook bauen" },
+      ],
+      counter: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
