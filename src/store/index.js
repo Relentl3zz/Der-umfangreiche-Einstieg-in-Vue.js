e1b3a6800c8915c32bbdcdbb36f209bd0e3b0290
b.derksen@mail.de <b.derksen@mail.de>
2020-05-23 13:16:08 +0200

    Lektion 32: Umzug in den VueX Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      { id: 1, title: "HTML, CSS & JS lernen", done: false },
+      { id: 2, title: "Vue lernen", done: false },
+      { id: 3, title: "Next facebook bauen", done: true },
+      { id: 4, title: "Vue aufgabe erledigt", done: true },
+      { id: 5, title: "Vue computed properties gelernt", done: true },
+    ],
+    counter: 6,
+    searchQuery: "",
   },
   mutations: {
+    ADD_TODO(e) {
+      this.todos.push({ id: this.counter++, title: e.target.value });
+      e.target.value = "";
+    },
+    DELETE_TODO(id) {
+      this.todos.splice(
+        this.todos.findIndex((item) => item.id === id),
+        1
+      );
+    },
   },
-  actions: {
+  actions: {},
+  getters: {
+    activeToDos: (state) => {
+      return state.todos.filter((item) => !item.done);
+    },
+    doneToDos: (state) => {
+      return state.todos.filter((item) => item.done);
+    },
+    filteredToDos: (state) => {
+      return state.todos.filter((item) =>
+        item.title.toLowerCase().startsWith(state.searchQuery.toLowerCase())
+      );
+    },
   },
-  modules: {
-  }
-})
+});
--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -4,8 +4,8 @@
     <input v-on:keyup.enter="addToDo" />
 
     <h2>Search Results</h2>
-    <input v-model="searchQuery" />
-    <div v-if="searchQuery">
+    <input v-model="$store.state.searchQuery" />
+    <div v-if="$store.state.searchQuery">
       <ToDoItem
         v-for="todo in filteredToDos"
         :key="todo.id"
@@ -42,42 +42,15 @@ export default {
   components: {
     ToDoItem,
   },
-  data() {
-    return {
-      todos: [
-        { id: 1, title: "HTML, CSS & JS lernen", done: false },
-        { id: 2, title: "Vue lernen", done: false },
-        { id: 3, title: "Next facebook bauen", done: true },
-        { id: 4, title: "Vue aufgabe erledigt", done: true },
-        { id: 5, title: "Vue computed properties gelernt", done: true },
-      ],
-      counter: 6,
-      searchQuery: "",
-    };
-  },
-  methods: {
-    addToDo(e) {
-      this.todos.push({ id: this.counter++, title: e.target.value });
-      e.target.value = "";
-    },
-    deleteToDo(id) {
-      this.todos.splice(
-        this.todos.findIndex((item) => item.id === id),
-        1
-      );
-    },
-  },
   computed: {
     activeToDos() {
-      return this.todos.filter((item) => !item.done);
+      return this.$store.getters.activeToDos;
     },
     doneToDos() {
-      return this.todos.filter((item) => item.done);
+      return this.$store.getters.doneToDos;
     },
     filteredToDos() {
-      return this.todos.filter((item) =>
-        item.title.toLowerCase().startsWith(this.searchQuery.toLowerCase())
-      );
+      return this.$store.getters.filteredToDos;
     },
   },
 };
